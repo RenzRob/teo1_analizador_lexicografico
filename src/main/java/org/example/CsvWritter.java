d3e19fbd78c81a75b991fddd411a90084793efd9
renzorobles <renzomartinrobles99@gmail.com>
2024-10-09 14:01:18 -0300

    feat: eliminando comentarios y archivos sin usar

--- src/main/java/org/example/CsvWritter.java
+++ src/main/java/org/example/CsvWritter.java
@@ -10,22 +10,20 @@ import java.util.List;
 public class CsvWritter {
     public static void escribirSimbolosCSV(List<Simbolo> listaSimbolos, String archivoCSV) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivoCSV))) {
-            // Escribir encabezados del CSV
             bw.write("Nombre,Token,Tipo,Valor,Length");
             bw.newLine();
 
-            // Recorrer la lista de Simbolo y escribir cada instancia en el archivo
             for (Simbolo simbolo : listaSimbolos) {
                 bw.write( simbolo.getNombre()+ "," +
                         simbolo.getToken() + "," +
                         simbolo.getTipo() + "," +
                         simbolo.getValor() + "," +
                         simbolo.getLength());
-                bw.newLine();  // Ir a la siguiente línea
+                bw.newLine();
             }
 
         } catch (IOException e) {
-            e.printStackTrace();  // Manejar error en caso de que haya un problema con la escritura
+            e.printStackTrace();
         }
     }
 }
